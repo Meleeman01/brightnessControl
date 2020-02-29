de245c381baf344535856e24df77c1fdbc3ce26f
Meleeman01 <meleeman01@live.com>
2020-02-29 13:05:02 -0700

    added backlight to init script and eliminated need for sudo command

--- install.sh
+++ install.sh
@@ -2,4 +2,18 @@
 #install to /usr/local/bin
 
 cp ./backlight /usr/local/bin/backlight
+
+
+cd /etc/init.d
+touch backlight
+chmod o+x /etc/init.d/backlight 
+chmod g+x /etc/init.d/backlight
+
+BACKLIGHT_FILE="/etc/init.d/backlight"
+
+echo "#! bin/sh
+	  
+chmod o+w /sys/class/backlight/intel_backlight/brightness
+" > $BACKLIGHT_FILE;
+update-rc.d backlight defaults
 echo 'installed to /usr/local/bin! now type `backlight`'
